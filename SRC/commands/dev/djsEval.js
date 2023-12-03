9df7a9375b41a358db86462a99adc068f6275177
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-12-03 01:46:36 +0100

    Create djsEval.js

---
+++ SRC/commands/dev/djsEval.js
@@ -0,0 +1,10 @@
+module.exports = ({
+    name: "eval",
+    aliases: ['djs'],
+    description: "Allows the bot owner to execute any discord.js code",
+    usage: "djseval <code>",
+    code: ` 
+    $onlyForUsers[;705306248538488947;392609934744748032]
+    $if[$authorID==705306248538488947;Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>;]                
+    $djsEval[$message]
+`})
