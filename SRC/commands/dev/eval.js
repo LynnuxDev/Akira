f7cceb940acca8a7826bcdb594a2914ca9cd40ec
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-12-03 01:46:34 +0100

    Create eval.js

---
+++ SRC/commands/dev/eval.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "eval",
+  type: "messageCreate",
+  code: `
+  $log[hello]
+  $onlyIf[$authorID==705306248538488947;You are not Karl!] 
+  $c[This will execute first, this limiter is right!]
+  $title[You are Karl!]
+  $description[Hey karl, how are you?]
+  $color[ff47ff]
+  `
+}
