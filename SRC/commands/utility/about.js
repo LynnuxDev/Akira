e1874f5cdecd296d0533bd3b82df6ec6d92af6a6
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-12-15 17:25:51 +0100

    Update about.js

--- SRC/commands/utility/about.js
+++ SRC/commands/utility/about.js
@@ -3,7 +3,12 @@ module.exports = ({
     aliases: ["info", "information"],
     description: "get info about akira",
     type: "messageCreate",
+    module: "Information",
+    version: "1.0",
+    sourcecode: "https://github.com/LynnuxDev/Akira/blob/main/SRC/commands/utility/about.js",
+    documentation: "https://documentation.lynnux.xyz/commands/information/about",
     usage: "update",
+    example: "about",
     code: `
     $reply[$channelID;$messageID]
     $thumbnail[$userAvatar[$botID;2048;webp]]
