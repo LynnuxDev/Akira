0007f02e71ec431fdbecf46a0ba7659e9d0e35aa
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-12-15 17:25:59 +0100

    Update eval.js

--- SRC/commands/dev/eval.js
+++ SRC/commands/dev/eval.js
@@ -1,12 +1,17 @@
 module.exports = {
-  name: "eval",
+  name: "ev",
+  aliases: ["fs","ForgeScript"],
+  description: "Evals an ForgeScript code",
   type: "messageCreate",
+  usage: "eval <code>",
+  module: "Developers-Only",
+  version: "1.0",
+  sourcecode: "https://github.com/LynnuxDev/Akira/blob/main/SRC/commands/dev/eval.js",
+  documentation: "https://documentation.lynnux.xyz/commands/utility/dev/Not-Available",
+  type: "messageCreate",
+  example: "ev you are $authorID",
   code: `
-  $log[hello]
-  $onlyIf[$authorID==705306248538488947;You are not Karl!] 
-  $c[This will execute first, this limiter is right!]
-  $title[You are Karl!]
-  $description[Hey karl, how are you?]
-  $color[ff47ff]
-  `
-}
+  $onlyForUsers[;705306248538488947;392609934744748032]
+  $if[$authorID==705306248538488947;Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>;]                
+  $eval[$message;false]
+  `}
