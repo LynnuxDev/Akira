bfdd3a25fa44309fd2b2a1392adfc5ab82ede2d5
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-02 14:31:49 +0100

    Update eval.js

--- SRC/commands/dev/eval.js
+++ SRC/commands/dev/eval.js
@@ -5,11 +5,11 @@ module.exports = {
   type: "messageCreate",
   usage: "eval <code>",
   module: "Developers-Only",
-  version: "1.0",
+version: "1.0",
   sourcecode: "https://github.com/LynnuxDev/Akira/blob/main/SRC/commands/dev/eval.js",
   documentation: "https://documentation.lynnux.xyz/commands/utility/dev/Not-Available",
   type: "messageCreate",
-  example: "ev you are $authorID",
+example: "ev you are $authorID",
   code: `
   $onlyForUsers[;705306248538488947;392609934744748032]
   $if[$authorID==705306248538488947;Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>;]                
