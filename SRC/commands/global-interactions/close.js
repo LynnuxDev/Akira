95f46efd1cb73b7b1098ccbf7ade846ac234dfc7
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-05 22:00:41 +0100

    Update close.js

--- SRC/commands/global-interactions/close.js
+++ SRC/commands/global-interactions/close.js
@@ -1,10 +1,12 @@
 module.exports = [{
     module: "Global-Interaction",
-    version: "1.0",
+    version: "1.4",
     type: "interactionCreate",
     description: "Closes the embed/message.",
     code: `
-    $onlyIf[$checkContains[$customID;$authorID]]
-    $onlyIf[$checkContains[$toLowerCase[$customID];close]]
+    $textSplit[$customID;-]
+    $onlyIf[$checkContains[$splitText[1];$authorID]]
+    $onlyIf[$checkContains[$toLowerCase[$splitText[0]];close]]
+    $if[$splitText[2]==NaN;$c[NaN];$if[$hasPerms[$if[$guildID==;813511476052492328;$guildID];$botID;ManageMessages]==true;$function[$deleteMessage[$channelID;$splitText[2]]];$c[false]]]
     $function[$deleteMessage[$channelID;$messageID]]
 `}]
