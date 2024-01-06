e3eebfbe78071dccb3dfb627edede0ee2e8d4cca
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-06 18:11:15 +0100

    Update Akira.js

--- SRC/Akira.js
+++ SRC/Akira.js
@@ -60,27 +60,40 @@ top.commands.add({
     code: `
         $switch[$voteType;
             $case[test;
-                $log[A test vote was made by $username[$voteUserID] ($voteUserID)]
                 $let[user;$voteUserID]
                 $sendMessage[1193223146501836860;<@$get[user]> Voted For <@$clientID>!
                     $title[(test) Vote;https://top.gg/bot/$botID]
                     $description[<@$get[user]> Voted For <@$clientID>!]
                     $color[ff47ff]
-                    $addField[User:;<@$get[user]>]
-                    $addField[Bot:;<@$botID>]
-                    $addField[Voted at:;<t:$round[$math[$getTimestamp/1000]]:f>]
-                    $addField[Can vote again in:;<t:$math[$round[$math[$getTimestamp/1000]]+43200]:f>]
-                    $addField[Total Votes:;$totalVotes]
-                    $addField[Monthly Votes:;$monthlyVotes]
+                    $addField[User:;<@$get[user]>;true]
+                    $addField[Bot:;<@$botID>;true]
+                    $addField[Voted at:;<t:$round[$math[$getTimestamp/1000]]:f>;true]
+                    $addField[Can vote again in:;<t:$math[$round[$math[$getTimestamp/1000]]+43200]:R>;true]
+                    $addField[Total Votes:;$totalVotes;true]
+                    $addField[Monthly Votes:;$monthlyVotes;true]
                     $footer[This vote was a test.]
+                    $thumbnail[$userAvatar[$get[user]]]
                     $timestamp
                 ;false]
             ]
             $case[upvote;
-                if message is uwu
+                $let[user;$voteUserID]
+                $sendMessage[1193223146501836860;<@$get[user]> Voted For <@$clientID>!
+                    $title[Vote;https://top.gg/bot/$botID]
+                    $description[<@$get[user]> Voted For <@$clientID>!]
+                    $color[ff47ff]
+                    $addField[User:;<@$get[user]>;true]
+                    $addField[Bot:;<@$botID>;true]
+                    $addField[Voted at:;<t:$round[$math[$getTimestamp/1000]]:f>;true]
+                    $addField[Can vote again in:;<t:$math[$round[$math[$getTimestamp/1000]]+43200]:R>;true]
+                    $addField[Total Votes:;$totalVotes;true]
+                    $addField[Monthly Votes:;$monthlyVotes;true]
+                    $thumbnail[$userAvatar[$get[user]]]
+                    $timestamp
+                ;false]
             ]
             $default[
-                if $ case values werent matched
+                $log[\\[Error\\] | got a other vote type than upvote or test (got $voteType)]
             ]
         ]    
     `
