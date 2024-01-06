d59529a1c3280d2200f064286219ccbed5b6a22c
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-06 21:22:33 +0100

    Update Akira.js

--- SRC/Akira.js
+++ SRC/Akira.js
@@ -40,65 +40,7 @@ const client = new ForgeClient({
 // Load the commands
    client.commands.load("commands")
    client.applicationCommands.load("slash")
-   
-//////////////////////////////
-//  [   ForgeTop.gg    ]    //
-//////////////////////////////
-
-top.commands.add({
-    type: "error",
-    code: `$log[Error $postStatsError]`
-})
-
-top.commands.add({
-    type: "posted",
-    code: `$log[posted!]`
-})
-
-top.commands.add({
-    type: "voted",
-    code: `
-        $switch[$voteType;
-            $case[test;
-                $let[user;$voteUserID]
-                $sendMessage[1193223146501836860;<@$get[user]> Voted For <@$clientID>!
-                    $title[(test) Vote;https://top.gg/bot/$botID]
-                    $description[<@$get[user]> Voted For <@$clientID>!]
-                    $color[ff47ff]
-                    $addField[User:;<@$get[user]>;true]
-                    $addField[Bot:;<@$botID>;true]
-                    $addField[Voted at:;<t:$round[$math[$getTimestamp/1000]]:f>;true]
-                    $addField[Can vote again in:;<t:$math[$round[$math[$getTimestamp/1000]]+43200]:R>;true]
-                    $addField[Total Votes:;$totalVotes;true]
-                    $addField[Monthly Votes:;$monthlyVotes;true]
-                    $footer[This vote was a test.]
-                    $thumbnail[$userAvatar[$get[user]]]
-                    $timestamp
-                ;false]
-            ]
-            $case[upvote;
-                $let[user;$voteUserID]
-                $sendMessage[1193223146501836860;<@$get[user]> Voted For <@$clientID>!
-                    $title[Vote;https://top.gg/bot/$botID]
-                    $description[<@$get[user]> Voted For <@$clientID>!]
-                    $color[ff47ff]
-                    $addField[User:;<@$get[user]>;true]
-                    $addField[Bot:;<@$botID>;true]
-                    $addField[Voted at:;<t:$round[$math[$getTimestamp/1000]]:f>;true]
-                    $addField[Can vote again in:;<t:$math[$round[$math[$getTimestamp/1000]]+43200]:R>;true]
-                    $addField[Total Votes:;$totalVotes;true]
-                    $addField[Monthly Votes:;$monthlyVotes;true]
-                    $thumbnail[$userAvatar[$get[user]]]
-                    $timestamp
-                ;false]
-            ]
-            $default[
-                $log[\\[Error\\] | got a other vote type than upvote or test (got $voteType)]
-            ]
-        ]    
-    `
-})
-
+   top.commands.load("./topGG");
 
 ///////////////////////////////
 //  [   Client Login    ]    //
