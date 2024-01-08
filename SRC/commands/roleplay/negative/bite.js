2c94293284decd32a56d96f4c1feeee76f9842c3
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-08 12:37:20 +0100

    Update bite.js

--- SRC/commands/roleplay/negative/bite.js
+++ SRC/commands/roleplay/negative/bite.js
@@ -16,6 +16,9 @@ module.exports = {
         $let[message;$replace[$replace[$message;$message[0] ;];$message[0];]]
         $let[user1;**$username[$get[user]]**] $let[user2;**$username[$authorID]**]
 
+        $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];*]==false;:x: All roleplay commands are blocked by $get[user1].]
+        $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];bite]==false;:x: This roleplay command is blocked by $get[user1].]
+
         $onlyIf[$get[user]!=$authorID;You cant do this to yourself.]
         $onlyIf[$getVar[rp-blocked-$authorID;$get[user]]!=true;Your Blocked by this user]
 
