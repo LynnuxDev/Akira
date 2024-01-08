c081f0024b8e2e9a5530c238836bce4104369898
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-08 12:36:20 +0100

    Update boop.js

--- SRC/commands/roleplay/wholesome/boop.js
+++ SRC/commands/roleplay/wholesome/boop.js
@@ -12,10 +12,12 @@ module.exports = {
     code: `
         $onlyIf[$message!=;Wtong usage of this command use \`boop <user> {message}\`]
         $onlyIf[$checkContains[$message[0];<@&]!=true;You can't use this on a role.]
-
         $let[user;$if[$message[0]==;$authorID;$findUser[$message[0];true]]]
         $let[message;$replace[$replace[$message;$message[0] ;];$message[0];]]
         $let[user1;**$username[$get[user]]**] $let[user2;**$username[$authorID]**]
+
+        $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];*]==false;:x: All roleplay commands are blocked by $get[user1].]
+        $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];boop]==false;:x: This roleplay command is blocked by $get[user1].]
 
         $onlyIf[$get[user]!=$authorID;You cant do this to yourself.]
         $onlyIf[$getVar[rp-blocked-$authorID;$get[user]]!=true;Your Blocked by this user]
