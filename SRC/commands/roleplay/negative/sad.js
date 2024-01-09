1bcd757eb65a956bef47712b7882b1141b6ea01e
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-09 14:44:03 +0100

    Update sad.js

--- SRC/commands/roleplay/negative/sad.js
+++ SRC/commands/roleplay/negative/sad.js
@@ -17,8 +17,9 @@ module.exports = {
         $let[user1;**$username[$get[user]]**] $let[user2;**$username[$authorID]**]
 
         $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];*]==false;:x: All roleplay commands are blocked by $get[user1].]
+        $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];*]==false;:x: All roleplay commands are blocked by $get[user1].]
         $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];sad]==false;:x: This roleplay command is blocked by $get[user1].]
-
+    
         $onlyIf[$get[user]!=$authorID;You cant do this to yourself.]
         $onlyIf[$getVar[rp-blocked-$authorID;$get[user]]!=true;Your Blocked by this user]
 
