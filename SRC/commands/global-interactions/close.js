b2208f114668d66a9937c43faeee7d1a322aac81
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-09 14:38:14 +0100

    Update close.js

--- SRC/commands/global-interactions/close.js
+++ SRC/commands/global-interactions/close.js
@@ -5,8 +5,16 @@ module.exports = [{
     description: "Closes the embed/message.",
     code: `
     $textSplit[$customID;-]
-    $onlyIf[$checkContains[$splitText[1];$authorID]]
-    $onlyIf[$checkContains[$toLowerCase[$splitText[0]];close]]
-    $if[$splitText[2]==NaN;$c[NaN];$if[$hasPerms[$if[$guildID==;813511476052492328;$guildID];$botID;ManageMessages]==true;$function[$deleteMessage[$channelID;$splitText[2]]];$c[false]]]
-    $function[$deleteMessage[$channelID;$messageID]]
+    $c[
+        $splitText[0] == Custom ID Name
+        $splitText[1] == authorID of original author
+        $splitText[2] == Command MessageID 
+        - "EMBED" == ephemeral
+        - "NaN" == Not available 
+    ]
+    $onlyIf[$toLowerCase[$splitText[0]]==close;]
+    $onlyIf[$splitText[1]==$authorID;]
+
+    $if[$splitText[2]==NaN;$c[NaN];$if[$splitText[2]==EMBED;$interactionUpdate[:x: This message can't be deleted! Please click "Dismiss message" under the embed.];$if[$hasPerms[$if[$guildID==;813511476052492328;$guildID];$botID;ManageMessages]==true;$function[$deleteMessage[$channelID;$splitText[2]]];$c[false]]$function[$deleteMessage[$channelID;$messageID]]]]
+
 `}]
