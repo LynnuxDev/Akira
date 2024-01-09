875ec913f15daecbbed50b2147c6e74b78fd255a
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-09 14:38:27 +0100

    Update roleplay.js

--- SRC/commands/roleplay/roleplay.js
+++ SRC/commands/roleplay/roleplay.js
@@ -14,12 +14,13 @@ module.exports = {
     $let[type;$message[0]] $let[user;$if[$message[1]==;$authorID;$findUser[$message[1];true]]] $let[message;$replace[$replace[$message;$message[0] ;];$message[1] ;]]
     $let[user1;**$username[$get[user]]**] $let[user2;**$username[$authorID]**]
     
+    $onlyIf[$getVar[rp-blocked-$authorID;$get[user]]!=true;:x: Sorry but $get[user1] blocked you from using roleplay commands on you.$reply]
+
     $onlyIf[$get[user]!=$authorID;You cant do this to yourself.]
     $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];*]==false;:x: All roleplay commands are blocked by $get[user1].]
     $onlyIf[$checkContains[$getVar[rp-commandblocked;$get[user]];$get[type]]==false;:x: This roleplay command is blocked by $get[user1].]
-    $onlyIf[$getVar[rp-blocked-$authorID;$get[user]]!=true;Your Blocked by this user]
     $setVar[$get[type]-give;$authorID;$sum[$getVar[$get[type]-give;$authorID];1]]
-    $setVar[$get[type]-got;$get[user];$sum[$getVar[$get[type]-got;$get[user]];1]]
+    $setVar[$get[type]-got;$get[uniceser];$sum[$getVar[$get[type]-got;$get[user]];1]]
     $let[msg;
         $if[$get[type]==blush;**$get[user1]** made **$get[user2]** blush.;
         $if[$get[type]==boop;**$get[user1]** got booped by **$get[user2]**;
