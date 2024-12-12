6cc76d8265be4e25f68f04908df1da8ad9d802ea
Dark-LYNN <botdevlynn@gmailcom>
2024-12-12 23:17:34 +0100

    refactor: Added Function checkBotChannel for easy access/changing if needed.

--- src/functions/checkBotChannel.ts
+++ src/functions/checkBotChannel.ts
@@ -2,7 +2,7 @@ import { CustomFunction } from "@/types";
 
 const functions: CustomFunction[] = [
   {
-    name: "checkBotChannel",
+    name: "checkBotChannel", 
     code: `
       $onlyIf[$channelID==$getGuildVar[BotChannel;$guildID;$channelID];$getGlobalVar[BotChannelError]]
     `
