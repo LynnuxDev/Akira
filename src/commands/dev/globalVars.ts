52e78baeaee8940f4b25df26295c877a8bba3eda
Dark-LYNN <botdevlynn@gmailcom>
2024-12-14 17:16:10 +0100

    chore: Import from @/types.ts

--- src/commands/dev/globalVars.ts
+++ src/commands/dev/globalVars.ts
@@ -1,16 +1,4 @@
-interface Command {
-  name?: string;
-  aliases?: string[];
-  type: string;
-  description: string;
-  module: string;
-  sourcecode: string;
-  documentation?: string;
-  usage?: string;
-  example?: string;
-  version: string;
-  code: string;
-}
+import { Command } from '@/types';
 
 const commands: Command[] = [
   {
