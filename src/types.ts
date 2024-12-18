b62cc89cf32536af0629594c35d4a141505793cc
Dark-LYNN <botdevlynn@gmailcom>
2024-12-18 22:22:04 +0100

    chore: removed not needed part

--- src/types.ts
+++ src/types.ts
@@ -35,9 +35,3 @@ export interface customFunction {
 export interface Translations {
   [key: string]: any;
 }
-
-export interface CustomFunction {
-  name: string,
-  params?: string[],
-  code: string,
-}
