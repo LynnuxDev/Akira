7bcb8a2b9ac13f9689293fb651547b7f5d761e43
Dark-LYNN <botdevlynn@gmailcom>
2024-12-20 22:02:17 +0100

    chore: Added TS functions for easy usage.

--- src/native/calculatePercentage.ts
+++ src/native/calculatePercentage.ts
@@ -1,3 +1,9 @@
+/**
+ * Simply calculates the percentage of 2 values.
+ * @param value the given value
+ * @param maxValue the max value aka 100%
+ * @returns Return the calculated percentage.
+ */
 export function calculatePercentage(value: number, maxValue: number): number {
   if (maxValue === 0) {
     throw new Error("Max value cannot be zero");
