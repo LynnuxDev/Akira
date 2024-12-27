dbf1f8a34cded8da93f11dbc9769f52264e2b33f
Dark-LYNN <botdevlynn@gmailcom>
2024-12-27 21:01:06 +0100

    chore: Added {{client}} tag to customError function.

--- src/functions/customError.ts
+++ src/functions/customError.ts
@@ -14,7 +14,7 @@ const functions: CustomFunction[] = [
       $footer[Error code: "$get[error]" | Meaning: "$env[result;$get[error];meaning]"]
       $arrayLoad[title;,;$env[result;$get[error];title]]
       $title[$replace[$replace[$replace[$arrayRandomValue[title];";;2];\\];;1];[;;1]]
-      $description[$replace[$replace[$replace[$env[result;$get[error];description];{prefix};$if[$getUserVar[prefix;$get[author];false]!=false;$getUserVar[prefix;$get[author];a.];$getUserVar[prefix;$guildID;a.]];-1];{example};$commandInfo[messageCreate;$get[origin];usage];-1];{messageOne};$message[1];-1]]
+      $description[$replace[$replace[$replace[$replace[$env[result;$get[error];description];{{prefix}};$if[$getUserVar[prefix;$get[author];false]!=false;$getUserVar[prefix;$get[author];$getGlobalVar[prefix]];$getUserVar[prefix;$guildID;$getGlobalVar[prefix]]];-1];{example};$commandInfo[messageCreate;$get[origin];usage];-1];{messageOne};$message[1];-1];{{client}};$clientID;-1]]
     `
   }
 ];
