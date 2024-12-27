346df740035e2bc655f510ef97eebbb1d171fa2c
Dark-LYNN <botdevlynn@gmailcom>
2024-12-27 21:11:10 +0100

    refactor: onReady Event now uses types import.

--- src/commands/events/onReady.ts
+++ src/commands/events/onReady.ts
@@ -1,30 +1,14 @@
-interface Command {
-  name?: string;
-  aliases?: string[];
-  type: string;
-  description: string;
-  module: string;
-  sourcecode: string;
-  documentation?: string;
-  usage?: string;
-  example?: string;
-  version: string;
-  code: string;
-}
+import { Event } from '../../types';
 
-const commands: Command[] = [{
+const onReady: Event[] = [{
   name: 'onReady',
   type: 'ready',
   description: 'This command runs every time the bot becomes online.',
-  module: 'ClientSpecific',
-  sourcecode: 'https://github.com/LynnuxDev/Akira/blob/v1.0.0/src/commands/Global-Interactions/triggers/onReady.ts',
-  documentation: 'https://documentation.lynnux.xyz/commands/Client/Not-Available',
-  usage: 'n/a',
-  example: 'n/a',
+  module: 'Client',
   version: '1.0',
   code: `
     $logger[Info;$username[$clientID] | Running with "$commandCount" commands]
   `
 }];
 
-export default commands;
+export default onReady;
