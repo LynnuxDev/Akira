b8ee7edfde1fa6488d62ab1f558d7f98b7e2a15a
Dark-LYNN <botdevlynn@gmailcom>
2024-12-27 21:47:29 +0100

    refactor: containsCommand function now uses the commandInfo fs-native-function.

--- src/functions/containsCommand.ts
+++ src/functions/containsCommand.ts
@@ -5,7 +5,7 @@ const functions: CustomFunction[] = [
     name: 'containsCommand',
     params: ['command'],
     code: `
-      $return[$checkContains[$toLowerCase[$env[command]];blacklist;black-list;botchannel;bot-channel;default;default-channels;disable;disable-command;enable-command;enable;freechannel;free-channel;free;ignore;ignore-channel;listen;listen-channel;permission;permissions;perm;perms;white-list;whitelist;anime;animelookup;whattheanime;about;info;information;avatar;useravatar;cleardata;removedata;cmd;commandinfo;command-info;command;sos;help;help-me;what;howto;how-to;report;ifoundabug;ibrokesomething;settings;setting;setting;userinfo;user;whois]]
+      $return[$if[$commandInfo[messageCreate;$tolowercase[$env[command]];name]!=;true;false]]
     `
   }
 ];
