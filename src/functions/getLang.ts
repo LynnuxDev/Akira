5281110b9d3f9696ea143bed5c0100307e157776
Dark-LYNN <botdevlynn@gmailcom>
2024-12-27 21:08:56 +0100

    chore: getLang function now supports guilds.

--- src/functions/getLang.ts
+++ src/functions/getLang.ts
@@ -5,8 +5,17 @@ const functions: CustomFunction[] = [
     name: 'getLang',
     params: ['userID'],
     code: `
-      $let[user;$getUserVar[uuid;$customEncrypt[encrypt;$env[userID]]]]
-      $let[lang;$getUserVar[language;$get[user];en-us]]
+      $if[$findUser[$env[userId]]!=;
+        $let[user;$getUserVar[uuid;$customEncrypt[encrypt;$env[userID]]]]
+        $let[lang;$getUserVar[language;$get[user];en-us]]
+      ;
+        $if[$guildExists[$env[userID]]==true;
+          $let[guild;$getGuildVar[uuid;$customEncrypt[encrypt;$env[userID]]]]
+          $let[lang;$getGuildVar[language;$get[guild];en-us]]
+        ;
+        $let[lang;en-us]
+        ]
+      ]
       $return[$get[lang]]
     `
   }
