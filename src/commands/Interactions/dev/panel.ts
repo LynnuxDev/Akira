cd17dee78697948e3c08fe8ae13c689c86be466c
Dark-LYNN <botdevlynn@gmailcom>
2025-01-03 21:45:14 +0100

    fix: Dev panel.

--- src/commands/Interactions/dev/panel.ts
+++ src/commands/Interactions/dev/panel.ts
@@ -44,11 +44,11 @@ const Panel: InteractionCommand[] = [
       $interactionUpdate[
         $color[$getGlobalVar[color]]
         $title[Developer Panel:]
-        $addField[Server Info;- <:Website:1271544143423996005> Ip: ||$get[ip]||\n- <:Clock:1271543787637833759> Uptime: <t:$round[$math[$round[$math[$getTimestamp/1000];0]-$osUptime];0]:R>\n- <:CPU:1271543827395772517> CPU: \`$round[$get[cpuUsage];2]%\` - {$cpuCores}\n- <:Server:1271544093838938235> Ram: \`$ram[true;true]\`;true]
-        $addField[Process Info;- <:Clock:1271543787637833759> Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>\n- <:CPU:1271543827395772517> CPU: \`$cpuUsage[false]% {$cpuCores}\`\n- <:Server:1271544093838938235> Ram: \`$ram[true;false]\`;true]
+        $addField[Server Info;- <:Website:$if[$environment==main;1263809959267930213;1271544143423996005]> Ip: ||$get[ip]||\n- <:Clock:$if[$environment==main;1263809772478529576;1271543787637833759]> Uptime: <t:$round[$math[$round[$math[$getTimestamp/1000];0]-$osUptime];0]:R>\n- <:CPU:$if[$environment==main;1263809787661910050;1271543827395772517]> CPU: \`$round[$get[cpuUsage];2]%\` - {$cpuCores}\n- <:Server:$if[$environment==main;1263809924295819285;1271544093838938235]> Ram: \`$ram[true;true]\`;true]
+        $addField[Process Info;- <:Clock:$if[$environment==main;1263809772478529576;1271543787637833759]> Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>\n- <:CPU:$if[$environment==main;1263809787661910050;1271543827395772517]> CPU: \`$cpuUsage[false]% {$cpuCores}\`\n- <:Server:$if[$environment==main;1263809924295819285;1271544093838938235]> Ram: \`$ram[true;false]\`;true]
         $addField[;;true]
-        $addField[Client Info;**Client Stats:**\n- <:Roles:1271544076491292736> Guilds: \`$guildCount\`\n- <:members:1271543939576627322> Users: \`$userCount\`\n- <:shard:1271544119080259666> Shards: \`$shardsOnline/$shardCount\`\n- <:Plus:1271544047579693148> Commands: \`$commandCount\`;true]
-        $addField[<:Spacer:1275843251349356675>;<:Spacer:1275843251349356675>\n- <:Akira_Active_Dev:1271546151543373906> Commands used since startup: \`$getGlobalVar[startCommands]\`\n- <:ping:1271544029053452297> DB Latency: \`$dbPing\`\n- <:ping:1271544029053452297> WS Latency: \`$pingms\`\n- <:ping:1271544029053452297> RoundTrip Latency: \`$roundtrip\`;true]
+        $addField[Client Info;**Client Stats:**\n- <:Roles:$if[$environment==main;1263809915529728072;1271544076491292736]> Guilds: \`$guildCount\`\n- <:members:$if[$environment==main;1263809852145270926;1271543939576627322]> Users: \`$userCount\`\n- <:shard:$if[$environment==main;1263809940569456650;1271544119080259666]> Shards: \`$shardsOnline/$shardCount\`\n- <:Plus:1271544047579693148> Commands: \`$commandCount\`;true]
+        $addField[<:Spacer:$if[$environment==main;$if[$environment==main;1324808760207736842;1275843251349356675]>;<:Spacer:$if[$environment==main;$if[$environment==main;1324808760207736842;1275843251349356675]>\n- <:Akira_Active_Dev:$if[$environment==main;1263809801817821278;1271546151543373906]> Commands used since startup: \`$getGlobalVar[startCommands]\`\n- <:ping:$if[$environment==main;1263809890468499508;1271544029053452297]> DB Latency: \`$dbPing\`\n- <:ping:$if[$environment==main;1263809890468499508;1271544029053452297]> WS Latency: \`$pingms\`\n- <:ping:$if[$environment==main;1263809890468499508;1271544029053452297]> RoundTrip Latency: \`$roundtrip\`;true]
         $addField[;;true]
 
         $addActionRow
@@ -75,16 +75,42 @@ const Panel: InteractionCommand[] = [
         $thumbnail[$guildIcon[$guildID;2048;webp;false]]
         $description[**Guild Banned:** \`False\`]
         $addField[Bot Info:;
-shardID: \`$guildShardID\`
-Joined: \`Join time stamp\`
+- <:shard:$if[$environment==main;1263809940569456650;1271544119080259666]> shardID: \`$guildShardID\`
+- <:Akira_Active_Dev:$if[$environment==main;1263809794632974346;1271543843157971014]> Joined: \`Join time stamp\`
         ;true]
         $addField[Stats:;
-Slash Commands Used: \`NaN\`
-Message Commands Used: \`NaN\`
+- <:Date:$if[$environment==main;1263809801817821278;1271546151543373906]> Slash Commands Used: \`NaN\`
+- <:Date:$if[$environment==main;1263809801817821278;1271546151543373906]> Message Commands Used: \`NaN\`
         ;true]
 
         $addActionRow
-        $addButton[panelPrevGuild~$authorID~1;;Secondary;◀️;true]
+        $addButton[panelPrevGuild~$authorID~0;;Secondary;◀️;true]
+        $addButton[resetGuildPanel~$authorID;Reset;Danger;🗑️;false]
+        $addButton[banGuildPanel~$guildID;ban;Danger;🔨;false]
+        $addButton[panelNextGuild~$authorID~0;;Secondary;▶️;false]
+
+        $addActionRow
+        $addButton[close~$authorID~message~$messageID;Close;Danger;✖️]
+        $addButton[panelPrevious~$authorID;Developer;Secondary;;false]
+      ]
+    `
+  },
+  {
+    type: 'interactionCreate',
+    version: 'V1.0.0',
+    code: `
+      $textSplit[$customID;~]
+
+      $onlyIf[$splitText[0]==panelNextGuild;]
+      $onlyIf[$splitText[1]==$authorID;$ephemeral $customError[716;panel]]
+
+      $interactionUpdate[
+        $color[$getGlobalVar[color]]
+        $title[Developer Guild Panel:]
+        $thumbnail[$guildIcon[$guildID;2048;webp;false]]
+        $description[**Guild Permissions:**\n\`\`\`\n$memberPerms[$guildID;$clientID;,\n]\n\`\`\`]
+        $addActionRow
+        $addButton[panelGuild~$authorID~1;;Secondary;◀️;false]
         $addButton[resetGuildPanel~$authorID;Reset;Danger;🗑️;false]
         $addButton[banGuildPanel~$guildID;ban;Danger;🔨;false]
         $addButton[panelNextGuild~$authorID~1;;Secondary;▶️;true]
