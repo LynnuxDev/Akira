22087bb6e27e9417fa356f664766b0804e9d7c78
Dark-LYNN <botdevlynn@gmailcom>
2025-01-07 14:47:16 +0100

    refactor: Similar blocks of code found in 2 locations.

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,16 @@
 import { CommandType, IForgeFunction, IBaseCommand, IApplicationCommandData } from "@tryforge/forgescript";
+
+type ModuleList = 'clientSpecific'  | 'dev' | 'automation' | 'automod' | 'economy' | 'leveling' | 'moderation' | 'fun' | 'permissions' | 'premium' | 'profile' | 'reactionRoles' | 'roleplay' | 'search' | 'settings' | 'utility';
+type topggEvent = 'voted' | 'error' | 'posted';
+type ForgeEvent = 'ready' | 'guildCreate' | 'error' | 'messageCreate' | 'shardReady' | 'shardDisconnect';
+
 export interface CustomFunction extends IForgeFunction {
   version?: string;
 }
 
 export interface Command extends IBaseCommand<CommandType> {
   description?: string;
-  module: 'clientSpecific' | 'dev' | 'automation' | 'automod' | 'economy' | 'leveling' | 'moderation' | 'fun' | 'permissions' | 'premium' | 'profile' | 'reactionRoles' | 'roleplay' | 'search' | 'settings' | 'utility';
+  module: ModuleList
   sourcecode?: string;
   documentation?: string;
   usage?: string;
@@ -14,25 +19,24 @@ export interface Command extends IBaseCommand<CommandType> {
 }
 
 export interface Translations {
-  [key: string]: any;
+  [key: string]: string | string[] | { [key: string]: string };
 }
 
 export interface TopGG {
-  type: 'voted' | 'error' | 'posted';
+  type: topggEvent;
   code: string;
 }
 
-
 export interface Event {
-  type: 'ready' | 'guildCreate' | 'error' | 'messageCreate' | 'shardReady' | 'shardDisconnect';
+  type: ForgeEvent;
   name?: string;
   description?: string;
-  module: 'Client';
+  module: ModuleList;
   version: string;
   code: string;
 }
 
 export interface ISlash extends IApplicationCommandData {
   usage?: string;
-  module?: 'client' | 'dev' | 'automation' | 'automod' | 'economy' | 'leveling' | 'moderation' | 'fun' | 'permissions' | 'premium' | 'profile' | 'reactionRoles' | 'roleplay' | 'search' | 'settings' | 'utility';
+  module?: ModuleList
 }
