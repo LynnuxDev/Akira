dd70f63dd6b5a6f7013f278142b8204a3c47c764
Dark-LYNN <botdevlynn@gmailcom>
2025-01-07 02:11:58 +0100

    fix: Fixed error message showing user prefix for guild prefix.

--- src/functions/customError.ts
+++ src/functions/customError.ts
@@ -14,7 +14,7 @@ const functions: CustomFunction[] = [
       $footer[Error code: "$get[error]" | Meaning: "$env[result;$get[error];meaning]"]
       $arrayLoad[title;,;$env[result;$get[error];title]]
       $title[$replace[$replace[$replace[$arrayRandomValue[title];";;2];\\];;1];[;;1]]
-      $description[$replace[$replace[$replace[$replace[$replace[$replace[$env[result;$get[error];description];{{prefix}};$if[$getUserVar[prefix;$get[author];false]!=false;$getUserVar[prefix;$get[author];$getGlobalVar[prefix]];$getUserVar[prefix;$guildID;$getGlobalVar[prefix]]];-1];{example};$commandInfo[messageCreate;$get[origin];usage];-1];{messageOne};$message[1];-1];{{client}};$clientID;-1];{{command}};$commandInfo[messageCreate;$get[origin];name];-1];{{messageZero}};$message[0];-1]]
+      $description[$replace[$replace[$replace[$replace[$replace[$replace[$replace[$env[result;$get[error];description];{example};$commandInfo[messageCreate;$get[origin];usage];-1];{messageOne};$message[1];-1];{{client}};$clientID;-1];{{command}};$commandInfo[messageCreate;$get[origin];name];-1];{{messageZero}};$message[0];-1];{{botChannel}};$getGuildVar[BotChannel;$guildID;$channelID];-1];{{prefix}};$if[$get[origin]==server-set-prefix;$getGuildVar[prefix;$customEncrypt[encrypt;$guildID]];$getUserVar[prefix;$getUUID[$authorID]]];-1]]
     `
   }
 ];
